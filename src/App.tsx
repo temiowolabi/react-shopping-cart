39d9f019e754744d089082114086801e0b597ed4
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2022-01-28 11:46:26 +0000

    Shopping Cart Items Displaying on webpage and styled

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useQuery } from 'react-query'
 
 //Components
+import Item from './Item/Item'
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -29,10 +30,30 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
  
 function App() {
+  const [cartOpen, setCartOpen] = useState(false);
+  
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts); //products is a query key
     console.log(data);
+
+    const getTotalItems = () => null;
+
+    const handleAddToCart = (clickedItem: CartItemType) => null;
+
+    const handleRemoveFromCart = () => null;
+
+    if(isLoading) return <LinearProgress />;
+    if(error) return <div>Oops...Something went wrong :/</div>
   return (
-    <div className="App">Start</div>
+   <Wrapper>
+     <Grid container spacing={3}>
+       {data?.map(item => (
+         <Grid item key={item.id} xs{...12} sm{...4}>
+          <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+       ))}
+     </Grid>
+   </Wrapper>
+
   );
 }
 
